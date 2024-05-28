19d1eaa39622558f2bd88c2e17de65844e9b972a
Ayushi Katariya <76788711+Ayushi1521996@users.noreply.github.com>
2024-05-29 01:33:05 +0530

    SwapTwoNumber

---
+++ SwapWithoutExtra.java
@@ -0,0 +1,20 @@
+package practiceJava;
+
+import java.util.Scanner;
+
+public class SwapWithoutExtra {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		A= A+B;
+		B= A-B;
+		A= A-B;
+
+		System.out.println(A);
+		System.out.println(B);
+	}
+
+}
